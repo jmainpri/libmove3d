774468f61e0dc82fc78b1c6a81373174e5e5220e
Akin Sisbot <easisbot@smeagol.laas.fr>
2009-06-26 17:27:12 +0200

    New GIK files

---
+++ hri_planner/proto/FORMgikjointselection_proto.h
@@ -0,0 +1,16 @@
+/*
+ *  FORMgikjointselection_proto.h
+ *  XcodeBioMove3D
+ *
+ *  Created by Akin Sisbot on 25/6/09.
+ *
+ */
+
+#ifndef __CEXTRACT__
+
+extern void g3d_create_gik_jointsel_form ( void );
+extern void g3d_show_gik_jointsel_form ( void );
+extern void g3d_hide_gik_jointsel_form ( void );
+extern void g3d_delete_gik_jointsel_form ( void );
+
+#endif /* __CEXTRACT__ */
