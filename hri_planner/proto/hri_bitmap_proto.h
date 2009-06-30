399f21f1620db7419b573d969af71b9c7404b577
Thibault Kruse <kruset@in.tum.de>
2009-06-30 16:12:39 +0200

    HRI Added human moving state

--- hri_planner/proto/hri_bitmap_proto.h
+++ hri_planner/proto/hri_bitmap_proto.h
@@ -86,5 +86,6 @@ extern gnuplot_ctrl * hri_bt_init_gnuplot ( double xmin, double xmax, double ymi
 extern int hri_bt_gnuplot_bitmap ( gnuplot_ctrl * h, hri_bitmapset * btset, int btno, double exclude );
 extern hri_bitmapset* hri_bt_create_bitmapsworobots( void );
 extern int hri_set_human_state(hri_human * human,int state, configPt  config );
+extern int hri_set_human_state_SICK(hri_human * human, int state, configPt config, int adjustForSick );
 extern int hri_bt_insert_obsrobot(hri_bitmapset * btset, hri_bitmap* bitmap, p3d_rob* obj, p3d_env* env, double expand, double value, int manip);
 #endif /* __CEXTRACT__ */
