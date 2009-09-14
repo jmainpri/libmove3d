2c42930cdf3e4e8d2810604bdc20168c46a786ed
Jim Mainprice <jmainpri@arn.(none)>
2009-09-14 17:29:20 +0200

    Add comment on the save context class

--- userappli/CppApi/SaveContext.hpp
+++ userappli/CppApi/SaveContext.hpp
@@ -11,22 +11,59 @@
 #include "p3d/env.hpp"
 #include <vector>
 
+/**
+ * Saves the context of run and
+ * Associate a Statistic Data strucure
+ */
 class SaveContext {
 
 public:
+	/**
+	 * Constructor
+	 */
 	SaveContext();
+
+	/**
+	 * Destructor
+	 */
 	~SaveContext();
 
+	/**
+	 * Erases all contexts
+	 */
 	void clear();
 
-	Env* copyVariables(Env& d);
+	/**
+	 * returns the number of environments
+	 * Stored in the data structures
+	 */
 	unsigned int getNumberStored();
+
+	/**
+	 * Saves Current Environment
+	 * to Stack
+	 */
 	unsigned int saveCurrentEnvToStack();
+
+	/**
+	 * Changes to Current Environnement
+	 */
 	void switchCurrentEnvTo(unsigned int i);
 
+	/**
+	 * Prints the variables of
+	 * The environement
+	 */
 	void printVariables(unsigned int id);
+
+	/**
+	 * Print all variables of an environnement
+	 */
 	void printData(unsigned int i);
 
+	/**
+	 * Copy constructors of the 4 type of maps
+	 */
 	std::map<Env::boolParameter, boolContainer*> copyMap( std::map<Env::boolParameter, boolContainer*> map );
 	std::map<Env::intParameter, intContainer*> copyMap( std::map<Env::intParameter, intContainer*> map );
 	std::map<Env::doubleParameter, doubleContainer*> copyMap( std::map<Env::doubleParameter, doubleContainer*> map );
@@ -37,6 +74,10 @@ private:
 	std::vector< std::map<Env::intParameter, intContainer*> > _MapInt;
 	std::vector< std::map<Env::doubleParameter, doubleContainer*> > _MapDouble;
 	std::vector< std::map<Env::stringParameter, stringContainer*> > _MapString;
+
+	std::vector< std::vector<Statistics*> > _Statistics;
 };
+
+extern SaveContext storedContext;
 
 #endif /* SAVECONTEXT_HPP_ */
