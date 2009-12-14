97f929ff6d23a9f704c93e239b21a5edeafb0424
Jim Mainprice <jmainpri@c-3po.laas.fr>
2009-12-14 12:56:12 +0100

    New files for expansion

---
+++ planner_cxx/Diffusion/Expansion/RRTExpansion.h
@@ -0,0 +1,68 @@
+#ifndef RRTEXPANSION_HPP
+#define RRTEXPANSION_HPP
+
+#include "BaseExpansion.h"
+
+/**
+  * RRT Expansion Methods
+  *
+  * Functions to expand a tree in the CSpace
+  * Collision Checking, Random sampling and Biasing
+  */
+class RRTExpansion: public BaseExpansion
+{
+
+public:
+
+    RRTExpansion();
+    RRTExpansion(Graph* prtGraph);
+
+    ~RRTExpansion();
+
+
+    /**
+     * Shoots a direction (includes the biasing)
+     *
+     * @param Expanding component
+     * @param Goal Component
+     * @param Sampling passive mode
+     * @param Direction node
+     */
+    virtual std::tr1::shared_ptr<Configuration> getExpansionDirection(
+            Node* expandComp, Node* goalComp, bool samplePassive,
+            Node*& directionNode);
+
+    /**
+     * Gets the nearest node in the graph
+     *
+     * @param compNode Expanding component
+     * @param direction Direction of expansion
+     * @param Sampling passive mode
+     */
+    virtual Node* getExpansionNode(
+            Node* compNode, std::tr1::shared_ptr<Configuration> direction, int distance);
+
+    /**
+      * Basic Expansion Method
+      *
+      * @param expansionNode
+      * @param directionConfig
+      * @param directionNode
+      * @param method
+      */
+    int expandProcess(Node* expansionNode, std::tr1::shared_ptr<Configuration> directionConfig,
+                       Node* directionNode,
+                       Env::expansionMethod method);
+
+    /**
+     * Expands towards the goal
+     *
+     * @param expansionNode     Node expanded
+     * @param directionConfig   Direction
+     */
+    bool expandToGoal(Node* expansionNode,
+                      std::tr1::shared_ptr<Configuration> directionConfig);
+
+};
+
+#endif // RRTEXPANSION_HPP
