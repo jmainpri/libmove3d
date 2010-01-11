44092fa3188076de9b635506c71aaa49d08aeb5a
Jim Mainprice <jmainpri@c-3po.laas.fr>
2010-01-11 10:36:00 +0100

    Small changes in the hriCsMoPlanner part

--- planner/Diffusion/p3d_SpaceCost.c
+++ planner/Diffusion/p3d_SpaceCost.c
@@ -310,14 +310,12 @@ double p3d_GetConfigCost(p3d_rob* robotPt, configPt ConfPt)
         {
             if( ENV.getBool(Env::hriCsMoPlanner) )
             {
-//                Cost = HRICS_MOPL->getDistance()->getDistToZones()[0];
-//                p3d_set_and_update_robot_conf(QSaved);
-//                p3d_destroy_config(robotPt, QSaved);
+                  HRICS_MOPL->getDistance()->activateSafetyZonesMode();
+//                  HRICS_MOPL->getDistance()->getDistToZones()[0];
 #ifdef HRI_COSTSPACE
                 Cost = HRICS_MOPL->distanceToEntirePath();
 //                cout << "Cost =" << Cost << endl;
 #endif
-//                return Cost;
             }
             else
             {
