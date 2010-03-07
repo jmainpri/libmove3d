45090b1830c37ceab4d2c8841c704dec99c77f36
Mokhtar GHARBI <mgharbi@mokymac-wifi.laas.fr>
2010-03-07 15:28:13 +0100

    Add function to test whole Traj

--- planner/dpg/p3d_chanEnv.c
+++ planner/dpg/p3d_chanEnv.c
@@ -10,6 +10,109 @@
 static p3d_edge* p3d_getLpEdge(p3d_rob* robot, p3d_graph* graph, p3d_localpath* lp);
 
 
+int checkCollisionsOnPathAndReplan(p3d_rob* robot, p3d_traj* traj, p3d_graph* mainGraph, int optimized){
+  if (!traj) {
+    return 0;
+  }
+  p3d_rob* currentRobot = XYZ_ENV->cur_robot;
+  p3d_sel_desc_num(P3D_ROBOT, robot->num);
+  int ntest = 0;
+  bool graphTrajInCollision = false;
+  for (p3d_localpath *cur = traj->courbePt; cur != NULL && !graphTrajInCollision ; cur = cur->next_lp) {
+    if (p3d_unvalid_localpath_test(robot, cur, &ntest)) {//le lp est en collision
+      ChronoOn();
+      graphTrajInCollision = true;
+      configPt startConf = cur->config_at_distance(robot, cur, 0), endConf;
+      
+      //find the last node (end of a localpath)
+      p3d_localpath * tmpNext = cur;
+      do {
+        endConf = tmpNext->config_at_distance(robot, tmpNext, tmpNext->length_lp);
+        p3d_set_and_update_this_robot_conf(robot, endConf);
+        if (tmpNext->next_lp != NULL) {
+          tmpNext = tmpNext->next_lp;
+        }else{
+          break;
+        }
+      }while (p3d_col_test());
+      if(tmpNext){
+        tmpNext = tmpNext->prev_lp;
+      }
+      
+      //tagger les edges comme invalides.
+      p3d_edge* edge = p3d_getLpEdge(robot, mainGraph, cur);
+      p3d_unvalid_edge(mainGraph, edge);
+      for(p3d_localpath* tmplp = cur->next_lp; tmplp != tmpNext->next_lp; tmplp = tmplp->next_lp){
+        //if there is more than one edge, unvalidate the edges starting and ending at each node of the subpath
+        for(p3d_list_edge* le = edge->Nf->edges; le ; le = le->next){
+          p3d_unvalid_edge(mainGraph, le->E);
+        }
+        edge = p3d_getLpEdge(robot, mainGraph, tmplp);
+        p3d_unvalid_edge(mainGraph, edge);
+      }
+      p3d_addStartAndGoalNodeToGraph(startConf, robot->ROBOT_GOTO, cur->ikSol, robot->ikSolGoto, mainGraph, robot);
+      if(!p3d_graph_to_traj(robot)){
+        //separation du graph
+        p3d_separate_graph_for_unvalid_edges(robot->GRAPH);
+        //sauvegarde
+        int random = p3d_get_RANDOM_CHOICE();
+        int sampling = p3d_get_SAMPLING_CHOICE();
+        int motion = p3d_get_MOTION_PLANNER();
+        int biDirection = ENV.getBool(Env::biDir);
+        int nbTry = ENV.getInt(Env::NbTry);
+        int comp = ENV.getInt(Env::maxNodeCompco);
+        int nbNodes = p3d_get_NB_NODES();
+        p3d_set_RANDOM_CHOICE(P3D_RANDOM_SAMPLING);
+        p3d_set_SAMPLING_CHOICE(P3D_UNIFORM_SAMPLING);
+        p3d_set_MOTION_PLANNER(P3D_DIFFUSION);
+        ENV.setInt(Env::NbTry,10000000);
+        ENV.setBool(Env::biDir, true);
+        ENV.setInt(Env::maxNodeCompco,10000000);
+        p3d_set_NB_NODES(10000);
+        configPt qPos = robot->ROBOT_POS;
+        configPt qGoto = robot->ROBOT_GOTO;
+        robot->ROBOT_POS = startConf;
+        robot->ROBOT_GOTO = endConf;
+        int success = p3d_specific_search((char*)"");
+        //restore
+        robot->ROBOT_POS = qPos;
+        robot->ROBOT_GOTO = qGoto;
+        p3d_set_RANDOM_CHOICE(random);
+        p3d_set_SAMPLING_CHOICE(sampling);
+        p3d_set_MOTION_PLANNER(motion);
+        ENV.setInt(Env::NbTry,nbTry);
+        ENV.setBool(Env::biDir, biDirection);
+        p3d_set_NB_NODES(nbNodes);
+        ENV.setInt(Env::maxNodeCompco,comp);
+        if (!success){
+          printf("Impossible to avoid collision\n");
+          return -2;
+        }
+      }
+      cur = tmpNext;
+      PrintInfo(("Alternative Edge : "));
+      ChronoPrint("");
+      double tu = 0.0, ts = 0.0;
+      ChronoTimes(&tu, &ts);
+      ChronoOff();
+    }
+  }
+  if(graphTrajInCollision){ //If it's the optimized traj in collision, we change the trajectory with the graph traj.
+    p3d_addStartAndGoalNodeToGraph(robot->ROBOT_POS, robot->ROBOT_GOTO, robot->ikSolPos, robot->ikSolGoto, robot->GRAPH, robot);
+    if(p3d_graph_to_traj(robot)){
+      p3d_sel_desc_num(P3D_ROBOT, currentRobot->num);
+      return -1;
+    }else{
+      printf("oula Mino il y'a soucis!!! \n");
+      return -2;
+    }
+  }else{
+    p3d_sel_desc_num(P3D_ROBOT, currentRobot->num);
+    g3d_add_traj((char*)"Globalsearch", p3d_get_desc_number(P3D_TRAJ));
+    return 1;
+  }
+}
+
 // Return Values : -2 = Error  -1 = a collision is avoided  0 = No traj given  1 = Traj is collision Free.
 int replanForCollidingPath(p3d_rob* robot, p3d_traj* traj, p3d_graph* mainGraph, configPt current, p3d_localpath* currentLp, int optimized){
   if (!traj || !currentLp) {
@@ -21,10 +124,6 @@ int replanForCollidingPath(p3d_rob* robot, p3d_traj* traj, p3d_graph* mainGraph,
   double dist = 0;
   double trajLength =  p3d_compute_traj_length(traj);
   bool graphTrajInCollision = false;
-//if the trajectory is optimized, add it into the graph if not yet added
-//   if(optimized){
-//     p3dAddTrajToGraph(robot, mainGraph, traj);
-//   }
   //start form the localpath containing the current config of the robot
   p3d_localpath *cur = currentLp;
   double curDist = 0;
@@ -32,7 +131,7 @@ int replanForCollidingPath(p3d_rob* robot, p3d_traj* traj, p3d_graph* mainGraph,
     curDist += tmp->length_lp;
   }
   dist = curDist;
-  for (; cur != NULL && !graphTrajInCollision && dist < curDist + 0.33 * trajLength ; cur = cur->next_lp) {
+  for (; cur != NULL && !graphTrajInCollision /*&& dist < curDist + 0.33 * trajLength*/ ; cur = cur->next_lp) {
     if (p3d_unvalid_localpath_test(robot, cur, &ntest)) {//le lp est en collision
       ChronoOn();
       graphTrajInCollision = true;
--- planner/dpg/proto/p3d_chanEnv_proto.h
+++ planner/dpg/proto/p3d_chanEnv_proto.h
@@ -2,6 +2,7 @@
 #define __P3DCHANENV_H__
 //Enable DPG Flag in compilation to use
 
+extern int checkCollisionsOnPathAndReplan(p3d_rob* robot, p3d_traj* traj, p3d_graph* mainGraph, int optimized);
 extern int replanForCollidingPath(p3d_rob* robot, p3d_traj* traj, p3d_graph* mainGraph, configPt current, p3d_localpath* currentLp, int optimized);
 extern int checkForCollidingPath(p3d_rob* robot, p3d_traj* traj, p3d_localpath* currentLp);
 
