41171b75f6b33734595cbe54d532741e70e8cca1
Jim Mainprice <jmainpri@c-3po.laas.fr>
2010-04-12 16:43:59 +0200

    Sourlist in form

--- p3d/SourceList.cmake
+++ p3d/SourceList.cmake
@@ -1,5 +1,48 @@
 SET(BM3D_MODULE_NAME p3d)
 BM3D_INC_DIR_PROCESS (${BM3D_MODULE_NAME}/proto)
 BM3D_INC_DIR_PROCESS (${BM3D_MODULE_NAME})
-BM3D_SRC_SUBDIR_PROCESS(p3d_BB.c p3d_autocol.c p3d_config.c p3d_constraints.c p3d_del.c p3d_env.c p3d_get.c p3d_halton.c p3d_human_arm_ik.c p3d_ik.c p3d_ik_kuka.c p3d_ik_pa10.c p3d_info.c p3d_jacobian.c p3d_jnt_base.c p3d_jnt_fixed.c p3d_jnt_freeflyer.c p3d_jnt_knee.c p3d_jnt_plan.c p3d_jnt_rotate.c p3d_jnt_translate.c p3d_joints.c p3d_matrix.c p3d_parallel.c p3d_poly.c p3d_prim.c p3d_rlg.c p3d_copy_robot.cpp p3d_rw_env.c p3d_rw_jnt.c p3d_rw_scenario.c p3d_rw_traj.c p3d_rw_util.c p3d_set.c p3d_setpos.c polyhedre.c env.cpp)
+BM3D_SRC_SUBDIR_PROCESS(
+p3d_BB.c 
+p3d_autocol.c 
+p3d_config.c 
+p3d_del.c 
+p3d_env.c 
+p3d_get.c 
+p3d_halton.c 
+p3d_human_arm_ik.c 
+p3d_ik.c 
+p3d_ik_kuka.c 
+p3d_ik_pa10.c 
+p3d_info.c 
+p3d_jacobian.c 
+p3d_jnt_base.c 
+p3d_jnt_fixed.c 
+p3d_jnt_freeflyer.c 
+p3d_jnt_knee.c 
+p3d_jnt_plan.c 
+p3d_jnt_rotate.c 
+p3d_jnt_translate.c 
+p3d_joints.c 
+p3d_matrix.c 
+p3d_parallel.c 
+p3d_poly.c 
+p3d_prim.c 
+p3d_rlg.c 
+p3d_copy_robot.cpp 
+p3d_rw_env.c 
+p3d_rw_jnt.c 
+p3d_rw_scenario.c 
+p3d_rw_traj.c 
+p3d_rw_util.c 
+p3d_set.c 
+p3d_setpos.c 
+polyhedre.c 
+env.cpp)
+
+IF(P3D_COLLISION_CHECKING)
+BM3D_SRC_SUBDIR_PROCESS(
+p3d_constraints.c 
+)
+ENDIF(P3D_COLLISION_CHECKING)
+
 BM3D_QT_GENERATE_MOC(env.hpp)
