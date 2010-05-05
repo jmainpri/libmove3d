f25e5164f7beb8da324985ac47d2fa3e9751c24b
Jim Mainprice <jmainpri@c-3po.laas.fr>
2010-05-05 15:51:12 +0200

    Big in form softMotion

--- move3d/FORMsoftMotion.c
+++ move3d/FORMsoftMotion.c
@@ -165,7 +165,8 @@ static void CB_test_obj(FL_OBJECT *ob, long arg) {
 	rob = XYZ_ENV->cur_robot;
 	double x, y, z, rx, ry, rz;
 p3d_mat4ExtractPosReverseOrder2(rob->joints[9]->abs_pos, &x, &y, &z, &rx, &ry , &rz);
-printf("x %f %f %f %f %f %f\n",x, y, z, RADTODEG*rx, RADTODEG*ry, RADTODEG*rz);
+// RADTODEG does not exist watch out include files
+//printf("x %f %f %f %f %f %f\n",x, y, z, RADTODEG*rx, RADTODEG*ry, RADTODEG*rz);
 print_config_one_line_degrees(rob, rob->ROBOT_POS);
 	if(ob){fl_set_cursor(FL_ObjWin(ob), FL_DEFAULT_CURSOR);fl_set_button(ob,0);}
 }
