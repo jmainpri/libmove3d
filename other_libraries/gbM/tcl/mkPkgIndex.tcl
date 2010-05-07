d92296d61bff50a6036e3e86be0d291ce8f8f476
Xavier Broquere <xavier.broquere@laas.fr>
2010-05-07 10:50:30 +0200

    remove gbM and sofMotion-libs from the repo -> use packages from git at trac.laas.fr/git/robots

--- other_libraries/gbM/tcl/mkPkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-
-puts "Rebuilding package index in [pwd]"
-#pkg_mkIndex . gb.tcl gbModeles.tcl -load ../tclsrc/gb.so
-pkg_mkIndex -verbose . gb.tcl  pipo.tcl -load ../buildir/install/lib/gb.so
-exit 0
