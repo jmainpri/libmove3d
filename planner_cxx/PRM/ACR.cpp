351483c56d5959c5795a564741e5c597433eb9b5
Jim Mainprice <jmainpri@c-3po.laas.fr>
2010-06-08 16:13:29 +0200

    Modifies ACR and PRM

--- planner_cxx/PRM/ACR.cpp
+++ planner_cxx/PRM/ACR.cpp
@@ -29,17 +29,15 @@ ACR::~ACR()
 }
 
 
-void ACR::expandOneStep()
+/*void ACR::expandOneStep()
 {
 	//p3d_add_all_prm_node(_Graph->getGraphStruct(), _stop_func );
-
 	//N = p3d_APInode_shoot(_Graph->getGraphStruct());
 	
 	shared_ptr<Configuration> q = _Robot->shoot();
 		
 	if ( q->setConstraintsWithSideEffect() && (!q->IsInCollision()))
 	{
-		//Node* N = new Node();
 		p3d_node *N = NULL;
 		
 		N = p3d_create_node(_Graph->getGraphStruct());
@@ -56,66 +54,45 @@ void ACR::expandOneStep()
 	
 		if (N->numcomp == -1) 
 		{
-			/* Node have not been included in a compco, create one for it */
+			// Node have not been included in a compco, create one for it
 			p3d_create_compco(_Graph->getGraphStruct(), N);
 			p3d_merge_check(_Graph->getGraphStruct());
 		}
 	}
-	
-//	shared_ptr<Configuration> q = _Robot->shoot();
-//	
-//	if ( q->setConstraintsWithSideEffect() && (!q->IsInCollision()))
-//	{
-//		m_nbConscutiveFailures = 0;
-//		
-//		Node* N = new Node(_Graph,q);
-//		
-//		_Graph->insertNode(N);
-//		_Graph->linkToAllNodes(N);
-//		_Graph->MergeCheck();
 		
-		m_nbAddedNode ++;
+	m_nbAddedNode ++;
 		
-		if (ENV.getBool(Env::drawGraph))
-		{
-			(*_draw_func)();
-		}
-//	}
-//	else 
-//	{
-//		m_nbConscutiveFailures ++;
-//	}
-
-}
-
-/*uint ACR::expand(p3d_graph* Graph_Pt,int (*fct_stop)(void), void (*fct_draw)(void))
-{
-    if(ENV.getBool(Env::expandToGoal) &&
-       _Start->getConfiguration()->equal(*_Goal->getConfiguration()))
-    {
-        cout << "graph creation failed: start and goal are the same" << endl;
-        return(0);
-    }
-
-    int nbAddedNode = 0;
-
-    while(!this->checkStopConditions())
-    {
-        shared_ptr<Configuration> C = _Robot->shoot();
-        _nbConscutiveFailures ++;
-        if (!C->IsInCollision())
-        {
-            _nbConscutiveFailures = 0;
-            Node* N = new Node(_Graph,C);
-            _Graph->insertNode(N);
-            _Graph->linkToAllNodes(N);
-
-            nbAddedNode ++;
-            if (ENV.getBool(Env::drawGraph))
-            {
-                (*fct_draw)();
-            }
-        }
-    }
-    return nbAddedNode;
+	if (ENV.getBool(Env::drawGraph))
+	{
+		(*_draw_func)();
+	}
 }*/
+
+void ACR::expandOneStep()
+{
+	shared_ptr<Configuration> q = _Robot->shoot();
+	
+	if ( q->setConstraintsWithSideEffect() && (!q->IsInCollision()))
+	{
+		Node* newNode = new Node(_Graph,q,false);
+		
+		m_nbConscutiveFailures = 0;
+	
+		_Graph->insertNode(newNode);
+		_Graph->linkToAllNodes(newNode);
+		
+		if ( newNode->getNodeStruct()->numcomp == -1 ) 
+		{
+			/* Node have not been included in a compco, create one for it */
+			_Graph->createCompco(newNode);
+			_Graph->mergeCheck();
+		}
+	}
+	
+	m_nbAddedNode ++;
+	
+	if (ENV.getBool(Env::drawGraph))
+	{
+		(*_draw_func)();
+	}
+}
