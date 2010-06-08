2853df8b6fb933207ca796b191f5e73fa9416b76
Jean-Philippe Saut <jpsaut@laas.fr>
2010-06-08 17:13:07 +0200

    added Eigen:: to fix unrecognized Vector3d

--- planner/dpg/DpgCell.cpp
+++ planner/dpg/DpgCell.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-DpgCell::DpgCell(int i, Vector3d corner, API::ThreeDGrid* grid): API::ThreeDCell(i, corner, grid){
+DpgCell::DpgCell(int i, Eigen::Vector3d corner, API::ThreeDGrid* grid): API::ThreeDCell(i, corner, grid){
   _valid = 1;
   _edges.clear();
   _nodes.clear();
@@ -12,7 +12,7 @@ DpgCell::DpgCell(int i, Vector3d corner, API::ThreeDGrid* grid): API::ThreeDCell
 }
 
 void DpgCell::draw(int color, int width){
-  Vector3d corner = this->getCorner();
+  Eigen::Vector3d corner = this->getCorner();
   double xmin = corner[0], xmax = corner[0] + _cellSize[0], ymin = corner[1], ymax = corner[1] + _cellSize[1], zmin = corner[2], zmax = corner[2] + _cellSize[2];
   g3d_draw_simple_box(xmin, xmax, ymin, ymax, zmin, zmax, color, 0, width);
 }
