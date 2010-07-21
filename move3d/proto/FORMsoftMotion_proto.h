e5fb2760571cf5560bbad9d6f53860d59c639c6c
Amit Kumar Pandey <akpandey@verdier.laas.fr>
2010-07-21 15:26:40 +0200

    With JP, fixed bug of passing arguments for armPlanTask

--- move3d/proto/FORMsoftMotion_proto.h
+++ move3d/proto/FORMsoftMotion_proto.h
@@ -3,4 +3,4 @@ extern void g3d_show_soft_motion_form ( void );
 extern void g3d_hide_soft_motion_form ( void );
 extern void g3d_delete_soft_motion_form ( void );
 
-extern int p3d_optim_traj_softMotion(p3d_traj *trajPt, bool param_write_file, double *gain, int *ntest, std::vector <int> lp, std::vector < std::vector <double> > positions, int *nbPositions);
+extern int p3d_optim_traj_softMotion(p3d_traj *trajPt, bool param_write_file, double *gain, int *ntest, std::vector <int> &lp, std::vector < std::vector <double> > &positions);
