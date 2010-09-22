c700806114a674b4c81510305a44d4d4409bfe26
Jim Mainprice <jmainpri@c-3po.darty>
2010-09-22 20:44:47 +0200

    cleanning

--- qtWindow/qtMainInterface/SourceList.cmake
+++ qtWindow/qtMainInterface/SourceList.cmake
@@ -5,11 +5,6 @@ BM3D_SRC_SUBDIR_PROCESS(
 kcdpropertieswindow.cpp 
 mainwindow.cpp
 mainwindowTestFunctions.cpp
-qtMotionPlanner.cpp
-qtCost.cpp
-qtRobot.cpp
-qtUtil.cpp
-qdebugstream.cpp
 )
 
 BM3D_INC_DIR_PROCESS (${BM3D_MODULE_NAME})
@@ -18,31 +13,13 @@ BM3D_QT_GENERATE_MOC(
 kcdpropertieswindow.hpp 
 mainwindow.hpp
 mainwindowTestFunctions.hpp
-qtMotionPlanner.hpp
-qtCost.hpp
-qtRobot.hpp
-qtUtil.hpp
 )
 
 BM3D_QT_GENERATE_UI_HEADERS(
 kcdpropertieswindow.ui 
 mainwindow.ui 
-qtMotionPlanner.ui
-qtCost.ui
-qtRobot.ui
-qtUtil.ui
 )
 
-IF(HRI_COSTSPACE)
-BM3D_QT_GENERATE_MOC(
-qtHrics.hpp
-)
-BM3D_QT_GENERATE_UI_HEADERS(
-qtHrics.ui
-)
-BM3D_SRC_SUBDIR_PROCESS(
-qtHrics.cpp
-)
-ENDIF(HRI_COSTSPACE)
+include(${CMAKE_SOURCE_DIR}/${BM3D_MODULE_NAME}/sideWidgets/SourceList.cmake)
 
 SET(BM3D_MODULE_NAME ${BM3D_MODULE_NAME_TMP})
