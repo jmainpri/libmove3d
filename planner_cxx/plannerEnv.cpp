999d43e8bf655f7a0408027647e1148bc3a27992
Jim Mainprice <jmainpri@c-3po.darty>
2010-09-22 21:26:10 +0200

    Add plannerEnv variables

--- planner_cxx/plannerEnv.cpp
+++ planner_cxx/plannerEnv.cpp
@@ -20,90 +20,48 @@ using namespace std;
 
 // Definition of the parameter container
 Parameters<
-PlannerParameters::boolParameter,
-PlannerParameters::intParameter,
-PlannerParameters::doubleParameter,
-PlannerParameters::stringParameter,
-PlannerParameters::vectorParameter>* PlannerEnv = NULL;
+PlanParam::boolParameter,
+PlanParam::intParameter,
+PlanParam::doubleParameter,
+PlanParam::stringParameter,
+PlanParam::vectorParameter>* PlanEnv = NULL;
 
 // @brief Function that inizializes the 
 // Parameter container
 void initPlannerParameters()
 {
 	// Create 5 maps for all types
-	std::map<PlannerParameters::boolParameter,		boolContainer*>				myBoolMap;
-	std::map<PlannerParameters::intParameter,			intContainer*>				myIntMap;
-	std::map<PlannerParameters::doubleParameter,	doubleContainer*>			myDoubleMap;
-	std::map<PlannerParameters::stringParameter,	stringContainer*>			myStringMap;
-	std::map<PlannerParameters::vectorParameter,	vectorContainer*>			myVectorMap;
+	std::map<PlanParam::boolParameter,		boolContainer*>				myBoolMap;
+	std::map<PlanParam::intParameter,			intContainer*>				myIntMap;
+	std::map<PlanParam::doubleParameter,	doubleContainer*>			myDoubleMap;
+	std::map<PlanParam::stringParameter,	stringContainer*>			myStringMap;
+	std::map<PlanParam::vectorParameter,	vectorContainer*>			myVectorMap;
 	
 	// Fill the 5 maps
-	myBoolMap.insert(std::make_pair(PlannerParameters::one,			new boolContainer(false)));
-	myBoolMap.insert(std::make_pair(PlannerParameters::two,			new boolContainer(true)));
-	myBoolMap.insert(std::make_pair(PlannerParameters::three,		new boolContainer(false)));
-	myBoolMap.insert(std::make_pair(PlannerParameters::four,		new boolContainer(true)));
+	myBoolMap.insert(std::make_pair(PlanParam::isMaxDisNeigh,			new boolContainer(false)));
+	myBoolMap.insert(std::make_pair(PlanParam::isWeightedChoice,	new boolContainer(false)));
+	
+	myIntMap.insert(std::make_pair(PlanParam::tata,				new intContainer(5)));
+	
+	myDoubleMap.insert(std::make_pair(PlanParam::eleven,	new doubleContainer(11.0)));
 
-	myIntMap.insert(std::make_pair(PlannerParameters::five,				new intContainer(5)));
-	myIntMap.insert(std::make_pair(PlannerParameters::six,				new intContainer(6)));
-	myIntMap.insert(std::make_pair(PlannerParameters::seven,			new intContainer(7)));
-	myIntMap.insert(std::make_pair(PlannerParameters::eight,			new intContainer(8)));
+	myStringMap.insert(std::make_pair(PlanParam::titi,		new stringContainer("titi")));
 	
-	myDoubleMap.insert(std::make_pair(PlannerParameters::nine,			new doubleContainer(9.0)));
-	myDoubleMap.insert(std::make_pair(PlannerParameters::ten,				new doubleContainer(10.0)));
-	myDoubleMap.insert(std::make_pair(PlannerParameters::eleven,		new doubleContainer(11.0)));
-	myDoubleMap.insert(std::make_pair(PlannerParameters::twelve,		new doubleContainer(12.0)));
-
-#ifdef QT_LIBRARY
-	myStringMap.insert(std::make_pair(PlannerParameters::thirteen,	new stringContainer("thirteen")));
-	myStringMap.insert(std::make_pair(PlannerParameters::fifteen,		new stringContainer("fifteen")));
-	myStringMap.insert(std::make_pair(PlannerParameters::sixteen,		new stringContainer("sixteen")));
-	myStringMap.insert(std::make_pair(PlannerParameters::seventeen,	new stringContainer("seventeen")));
-#endif
+	std::vector<double> tutu;
+	tutu.push_back( 1 ); tutu.push_back( 8 );
 	
-	std::vector<double> a,b;
-	a.push_back( 1 ); a.push_back( 8 );
-	b.push_back( 1 ); b.push_back( 9 );
-	
-	myVectorMap.insert(std::make_pair(PlannerParameters::eighteen,			new vectorContainer(a)));
-	myVectorMap.insert(std::make_pair(PlannerParameters::nineteen,			new vectorContainer(b)));
+	myVectorMap.insert(std::make_pair(PlanParam::tutu,			new vectorContainer(tutu)));
 
 	// Make the new parameter container
-	PlannerEnv =  new Parameters<
-	PlannerParameters::boolParameter,
-	PlannerParameters::intParameter,
-	PlannerParameters::doubleParameter,
-	PlannerParameters::stringParameter,
-	PlannerParameters::vectorParameter>(
+	PlanEnv =  new Parameters<
+	PlanParam::boolParameter,
+	PlanParam::intParameter,
+	PlanParam::doubleParameter,
+	PlanParam::stringParameter,
+	PlanParam::vectorParameter>(
 															myBoolMap,
 															myIntMap,
 															myDoubleMap,
 															myStringMap,
 															myVectorMap);
-	
-
-	// Print all parameters
-	cout << "Init Test Env : " << endl;
-	cout << PlannerEnv->getBool(PlannerParameters::one) << endl;
-	cout << PlannerEnv->getBool(PlannerParameters::two) << endl;
-	cout << PlannerEnv->getBool(PlannerParameters::three) << endl;
-	cout << PlannerEnv->getBool(PlannerParameters::four) << endl;
-	
-	cout << PlannerEnv->getInt(PlannerParameters::five) << endl;
-	cout << PlannerEnv->getInt(PlannerParameters::six) << endl;
-	cout << PlannerEnv->getInt(PlannerParameters::seven) << endl;
-	cout << PlannerEnv->getInt(PlannerParameters::eight) << endl;
-	
-	cout << PlannerEnv->getDouble(PlannerParameters::nine) << endl;
-	cout << PlannerEnv->getDouble(PlannerParameters::ten) << endl;
-	cout << PlannerEnv->getDouble(PlannerParameters::eleven) << endl;
-	cout << PlannerEnv->getDouble(PlannerParameters::twelve) << endl;
-#ifdef QT_LIBRARY
-	cout << PlannerEnv->getString(PlannerParameters::thirteen).toStdString() << endl;
-	cout << PlannerEnv->getString(PlannerParameters::fifteen).toStdString() << endl;
-	cout << PlannerEnv->getString(PlannerParameters::sixteen).toStdString() << endl;
-	cout << PlannerEnv->getString(PlannerParameters::seventeen).toStdString() << endl;
-#endif
-//	
-//	cout << PlannerEnv.getVector(PlannerParameters::eighteen) << endl;
-//	cout << PlannerEnv.getVector(PlannerParameters::nineteen) << endl;
 }
