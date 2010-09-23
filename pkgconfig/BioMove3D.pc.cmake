62d53a5ba74e096f8e03e6f1d8c6eb66ea628640
Emrah Akin Sisbot <easisbot@jidotop.laas.fr>
2010-09-23 13:53:32 +0200

    pkgconfig for BioMove3D

---
+++ pkgconfig/BioMove3D.pc.cmake
@@ -0,0 +1,13 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@CMAKE_INSTALL_PREFIX@/lib
+includedir=@CMAKE_INSTALL_PREFIX@/include
+BioMove3DFlags= @MOVE3D_COMPILATION_FLAGS@
+BioMove3DIncludes=@MOVE3D_COMPILATION_INCLUDES@
+BioMove3DLibs=@MOVE3D_COMPILATION_LIBS@
+ 
+Name: BioMove3D
+Description: Motion Planning Platform
+Version: @BIOMOVE3D_VERSION@
+Libs: -rpath,${BioMove3DLibs} -L${libdir} -lm3d
+Cflags: -I${includedir}/BioMove3D/include -I${BioMove3DIncludes} ${BioMove3DFlags} 
