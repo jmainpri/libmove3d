7d1816ebf040c2abeff0086d9eadf9cfb7b49d96
jmainpri <jmainpri@localhost.localdomain>
2010-10-21 13:34:49 +0200

    Renaming

---
+++ pkgconfig/Move3D-core.pc.cmake
@@ -0,0 +1,13 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@CMAKE_INSTALL_PREFIX@/lib
+includedir=@CMAKE_INSTALL_PREFIX@/include
+BioMove3DFlags= @MOVE3D_COMPILATION_FLAGS@
+BioMove3DIncludes=@MOVE3D_COMPILATION_INCLUDES@
+BioMove3DLibs=@MOVE3D_COMPILATION_LIBS@
+ 
+Name: BioMove3D
+Description: Motion Planning Platform
+Version: @BIOMOVE3D_VERSION@
+Libs: ${BioMove3DLibs} -L${libdir} -looMove3D-core
+Cflags: -I${includedir}/ooMove3D-core/include -I${BioMove3DIncludes} ${BioMove3DFlags} 
