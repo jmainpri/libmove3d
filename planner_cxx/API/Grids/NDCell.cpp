7007d96e8367e3671673ae3d5a82097186f5344c
Jim Mainprice <jmainpri@laas.fr>
2010-11-29 13:54:49 +0100

    Remove Qt Window, Hri costspace and CXX planner are now in separate libs

--- planner_cxx/API/Grids/NDCell.cpp
+++
@@ -1,5 +0,0 @@
-#include "NDCell.hpp"
-
-nDimCell::nDimCell()
-{
-}
--- planner_cxx/untitled.h
+++
@@ -1,9 +0,0 @@
-/*
- *  untitled.h
- *  
- *
- *  Created by Jim Mainprice on 05/12/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
-
