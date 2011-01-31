fc9ea8b9ccdb14cbd5f98a33a437ce452515c51b
JIM MAINPRICE <jmainpri@laas.fr>
2011-01-31 11:23:33 +0100

    Set draw cost features under the MOVE3D-CORE flag

--- graphic/g3d_draw_env.c
+++ graphic/g3d_draw_env.c
@@ -1331,10 +1331,11 @@ void g3d_draw_env_custom()
   win = g3d_get_cur_win();
 	
   p3d_drawRobotMoveMeshs();
-//#if defined( MOVE3D_CORE ) 
-#if defined ( CXX_PLANNER )
+  
+#if defined( MOVE3D_CORE ) 
   ext_g3d_draw_cost_features();
 #endif
+  
 #ifdef DPG
   if(XYZ_GRAPH && XYZ_GRAPH->dpgGrid){
     XYZ_GRAPH->dpgGrid->draw();
