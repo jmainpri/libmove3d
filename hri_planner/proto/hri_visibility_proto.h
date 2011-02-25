71c9fee6b61343d419d31d1ecd316e4d184127d7
Matthieu Warnier <mwarnier@laas.fr>
2011-02-25 16:51:59 +0100

    improve visibility and disappear management.
    - Disappear is tested through a new functions that process object percentage visibility.
    - Visibiliy threshold and human field of view for visibility processing are changed to avoid visibility always false if object is too far.
    - Change visibility processing for robot. ( We only check in current field of view and not in the whole pan tilt amplitude as for other agents)

--- hri_planner/proto/hri_visibility_proto.h
+++ hri_planner/proto/hri_visibility_proto.h
@@ -1,9 +1,9 @@
 extern void hri_initialize_visibility();
 extern int hri_is_object_visible(HRI_AGENT * agent,p3d_rob *object, int threshold, int save, int draw_at_end);
 extern int g3d_is_object_visible_from_viewpoint(p3d_matrix4 camera_frame, double camera_fov, p3d_rob *object, double *result, int save);
-extern int g3d_get_given_entities_pixelpresence_from_viewpoint(p3d_matrix4 camera_frame, double camera_fov, HRI_ENTITY **objects, int objects_nb, double *results, int save);
+extern int g3d_get_given_entities_pixelpresence_from_viewpoint(p3d_matrix4 camera_frame, double camera_fov, HRI_ENTITY **objects, int objects_nb, bool display_others_in_blue,double *results, int save);
 extern int g3d_is_object_visible_from_current_viewpoint(g3d_win* win, p3d_rob *object, double *result, int save, char *path);
-extern int g3d_get_given_entities_pixelpresence_in_current_viewpoint(g3d_win* win, HRI_ENTITY **objects, int objects_nb, double *vis_results, int save, char *path);
+extern int g3d_get_given_entities_pixelpresence_in_current_viewpoint(g3d_win* win, HRI_ENTITY **objects, int objects_nb, bool display_others_in_blue,double *vis_results, int save, char *path);
 extern int g3d_visibility_for_given_objects_in_current_viewpoint_pixelpercentage(g3d_win* win, p3d_rob **objects, int objects_nb, double *res, int save, char *path);
 extern int hri_object_visibility_placement(HRI_AGENT *agent, p3d_rob *object, int *result, double *elevation, double *azimuth);
 extern int hri_entity_visibility_placement(HRI_AGENT *agent, HRI_ENTITY *ent, int *result, double *elevation, double *azimuth);
@@ -21,3 +21,5 @@ extern int g3d_is_object_visible_from_current_viewpoint2(g3d_win* win, p3d_rob *
 extern int hri_compute_agent_sees(HRI_AGENT * agent, int threshold, int save, int draw_at_end);
 extern int hri_turn_agent_head_direction(HRI_AGENT *agent, double elevation, double azimuth);
 extern int g3d_compute_visibility_for_given_entities(HRI_ENTITY ** ents, HRI_AGENT * agent, HRI_VISIBILITY * res, int res_nb);
+extern int g3d_compute_visibility_in_fov_for_given_entities(HRI_ENTITY ** ents,HRI_ENTITY * agent_entity , HRI_AGENT * agent, HRI_VISIBILITY * res, int res_nb);
+extern int g3d_compute_visibility_in_fov_for_suspect_undetected_entity(HRI_ENTITIES * ents, int suspect_undetected_entity_index,HRI_AGENT * agent,HRI_AGENTS * agents);
