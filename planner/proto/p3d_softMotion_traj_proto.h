02d520c7dd74d362de45fb6c7658b3d15caa2c6a
Xavier Broquere <xavier.broquere@laas.fr>
2011-03-10 21:15:26 +0100

    declare p3d_getQSwitchIDFromMidCVS in header

--- planner/proto/p3d_softMotion_traj_proto.h
+++ planner/proto/p3d_softMotion_traj_proto.h
@@ -21,4 +21,7 @@ extern int p3d_optim_traj_softMotion(p3d_traj *trajPt, bool param_write_file, do
 extern int p3d_convert_traj_to_softMotion(p3d_traj *trajPt, bool param_write_file, bool approximate, std::vector <int> &lp, std::vector < std::vector <double> > &positions, SM_TRAJ &smTraj);
 
 extern void draw_trajectory_ptp();
+
+extern int p3d_getQSwitchIDFromMidCVS(double tau, double t_rep, int* id);
+
 #endif
