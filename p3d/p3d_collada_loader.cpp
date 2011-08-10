98314abaf22252a03105d8481191ae4e816bcca2
Lancelot Francois <francois.lancelot@laas.fr>
2011-08-10 16:23:14 +0200

    Add comments and rename functions

--- p3d/p3d_collada_loader.cpp
+++ p3d/p3d_collada_loader.cpp
@@ -61,7 +61,9 @@ p3d_collada_loader::~p3d_collada_loader()
 
 bool p3d_collada_loader::load(const char* filename)
 {
-	// Initialisation des variables ColladaDom et ouverture du fichier
+	bool chargement_reussi = true;
+
+	// Initialise les variables ColladaDom et ouvre le fichier
 	m_collada = new DAE;
 	m_root = m_collada->open(filename);
 
@@ -70,19 +72,16 @@ bool p3d_collada_loader::load(const char* filename)
 		return false;
 	}
 
-	scale = 1;
-    if( !!m_root->getAsset() ) {
+	// Récupère l'échelle de la scène
+	if( !!m_root->getAsset() ) {
         if( !!m_root->getAsset()->getUnit() ) {
         	scale = m_root->getAsset()->getUnit()->getMeter();
         }
     }
-    cout << "Echelle" << scale << endl;
-	//cout << "L'ouverture du fichier Collada a réussi" << endl;
 
-
-	// TODO initialisation du début de la description et de l'environnement à la main
-	cout << "p3d_beg_desc P3D_ENV humanTestEnv " << endl;
-	p3d_beg_desc(P3D_ENV, (char *)"humanTestEnv");
+	// Initialise le début de la description et de l'environnement à la main
+	cout << "p3d_beg_desc P3D_ENV Env " << endl;
+	p3d_beg_desc(P3D_ENV, (char *)"Env");
 
 	cout << "p3d_beg_desc P3D_OBSTACLE floor" << endl;
 	p3d_beg_desc(P3D_OBSTACLE, (char *)"floor");
@@ -93,14 +92,16 @@ bool p3d_collada_loader::load(const char* filename)
 	cout << "p3d_end_desc" << endl;
 	p3d_end_desc();
 
-	// Convertion du fichier collada dans les structures p3d
-	extract();
+	chargement_reussi = extractAndFill();
 
 	cout << "p3d_end_desc" << endl;
 	p3d_end_desc();
+
+	return chargement_reussi;
 }
 
-bool p3d_collada_loader::extract()
+
+bool p3d_collada_loader::extractAndFill()
 {
 	domCOLLADA::domSceneRef all_scene = m_root->getScene();
 	if(!all_scene)
@@ -108,13 +109,13 @@ bool p3d_collada_loader::extract()
 		cout << "Le chargement de la balise 'scene' a échoué" << endl;
 		return false;
 	}
-	// On récupère l'instance_visual_scene
+	// Récupère l'instance_visual_scene
 	if(all_scene->getInstance_visual_scene())
 	{
-		// Grâce à l'URL, on va directement à la description de la visual_scene dans la library_visual_scene
+		// Grâce à l'URL, conduit directement à la description de la visual_scene dans la library_visual_scene
 		domVisual_sceneRef visual_scene = daeSafeCast<domVisual_scene>(all_scene->getInstance_visual_scene()->getUrl().getElement().cast());
 
-		// On parcours tous les noeuds de la visual scene
+		// Parcourt tous les noeuds de la visual scene
 		for(size_t i=0; i < visual_scene->getNode_array().getCount(); i++)
 		{
 			domNodeRef node = visual_scene->getNode_array()[i];
@@ -122,19 +123,20 @@ bool p3d_collada_loader::extract()
 			cout << "p3d_beg_desc P3D_ROBOT" << endl;
 			p3d_beg_desc(P3D_ROBOT, (char*)"greiffer");
 
+			// TODO
+			// Ajout d'un FREEFLYER pour avoir un objet movable
 			//p3d_read_jnt_data * data;
 			//data = p3d_create_read_jnt_data(P3D_FREEFLYER);
 
-			// On parcours tous les noeuds fils
+			// Parcours tous les noeuds fils (sous-noeud, littleNode : node/node)
 			for(size_t j=0; j<node->getNode_array().getCount(); j++)
 			{
 				domNodeRef littleNode = node->getNode_array()[j];
 
-				// On affiche des informations sur ce noeud
 				cout << "p3d_beg_desc P3D_BODY " << littleNode->getID() << endl;
 				p3d_beg_desc(P3D_BODY, (char*)littleNode->getID());
 
-				extractGeometryMaterialPosition(littleNode);
+				extractAndFillGeometryPositionColor(littleNode);
 
 				cout << "p3d_end_desc"<< endl;
 				p3d_end_desc();
@@ -144,42 +146,95 @@ bool p3d_collada_loader::extract()
 			p3d_end_desc();
 		}
 	}
-
-
-
 	return true;
 }
 
-/// Paint the Geometry with the color material
-/// \param  pmat    Material info of the COLLADA's model
-/// \param  geom    Geometry properties in p3d
-void p3d_collada_loader::fillGeometryColor(const domMaterialRef pmat, const domNodeRef littleNode )
+void p3d_collada_loader::extractAndFillGeometryPositionColor(const domNodeRef littleNode)
 {
-    if( !!pmat && !!pmat->getInstance_effect() ) {
-        domEffectRef peffect = daeSafeCast<domEffect>(pmat->getInstance_effect()->getUrl().getElement().cast());
-        if( !!peffect ) {
-            domProfile_common::domTechnique::domPhongRef pphong = daeSafeCast<domProfile_common::domTechnique::domPhong>(peffect->getDescendant(daeElement::matchType(domProfile_common::domTechnique::domPhong::ID())));
-            if( !!pphong ) {
-                //if( !!pphong->getAmbient() && !!pphong->getAmbient()->getColor() ) {
-                  //  geom.ambientColor = getVector4(pphong->getAmbient()->getColor()->getValue());
-                //}
-                if( !!pphong->getDiffuse() && !!pphong->getDiffuse()->getColor() ) {
-                    domFx_color& diffuseColor = pphong->getDiffuse()->getColor()->getValue();
-                    // TODO à quoi sert le dernier indice des couleurs
-                    cout << "p3d_set_prim_color " << littleNode->getID() << " Any " << diffuseColor.get(0) << " " << diffuseColor.get(1) << " " << diffuseColor.get(2) << endl;
-            		p3d_poly *poly;
-            		double color_vect[3];
-            		poly = p3d_poly_get_poly_by_name ((char *)littleNode->getID());
-            		for(int i=0; i<3; i++)
-            			color_vect[i]=diffuseColor.get(i);
-            		p3d_poly_set_color(poly, Any, color_vect);
-                }
-            }
-        }
-    }
+	// Récupère les instances géométriques
+	for(size_t k=0; k<littleNode->getInstance_geometry_array().getCount(); k++)
+	{
+		cout << "p3d_add_desc_poly " << littleNode->getID() << " P3D_GRAPHIC" << endl;
+		p3d_add_desc_poly((char *)littleNode->getID(),P3D_GRAPHIC);
+
+		domInstance_geometryRef instance_geo = littleNode->getInstance_geometry_array()[k];
+
+		// Grâce à l'URL, conduit directement à la description de la géométrie dans la library_geometries
+		domGeometryRef geometry = daeSafeCast<domGeometry>(instance_geo->getUrl().getElement().cast());
+
+		if(geometry->getMesh())
+		{
+			// Récupère les vertices
+			domVerticesRef vertices = geometry->getMesh()->getVertices();
+			extractAndFillVertices(vertices);
+
+			// Récupère les polylist
+			for(size_t l=0; l<geometry->getMesh()->getPolylist_array().getCount(); l++)
+			{
+				domPolylistRef polylist = geometry->getMesh()->getPolylist_array()[l];
+				extractAndFillPolylist(polylist);
+			}
+			// Récupère les triangles
+			for(size_t l=0; l<geometry->getMesh()->getTriangles_array().getCount(); l++)
+			{
+				domTrianglesRef triangles = geometry->getMesh()->getTriangles_array()[l];
+				extractAndFillTriangles(triangles);
+			}
+			cout << "p3d_end_desc_poly" << endl;
+			p3d_end_desc_poly();
+
+			// TODO à revoir
+			// Ajoute du matérial (couleur) de cette instance_geometry
+			if (!!instance_geo->getBind_material() && !!instance_geo->getBind_material()->getTechnique_common()) {
+				const domInstance_material_Array& matarray = instance_geo->getBind_material()->getTechnique_common()->getInstance_material_array();
+				for (size_t imat = 0; imat < matarray.getCount(); ++imat) {
+					domMaterialRef p_mat = daeSafeCast<domMaterial>(matarray[imat]->getTarget().getElement());
+					if (!!p_mat) {
+						extractAndFillColor(p_mat, littleNode);
+					}
+				}
+			}
+		}
+		// Ajout de la position du littleNode
+		if(littleNode)
+		{
+			float x=0, y=0, z=0;
+			float rx=0, ry=0, rz=0;
+			// Translation
+			for(size_t n_translate=0; n_translate < littleNode->getTranslate_array().getCount(); n_translate++)
+			{
+				x+=littleNode->getTranslate_array()[n_translate]->getValue()[0];
+				y+=littleNode->getTranslate_array()[n_translate]->getValue()[1];
+				z+=littleNode->getTranslate_array()[n_translate]->getValue()[2];
+			}
+			// Rotation
+			for(size_t n_rotate=0; n_rotate < littleNode->getRotate_array().getCount(); n_rotate++)
+			{
+				if(littleNode->getRotate_array()[n_rotate]->getValue()[0]==1)
+					rx+=littleNode->getRotate_array()[n_rotate]->getValue()[3];
+				if(littleNode->getRotate_array()[n_rotate]->getValue()[1]==1)
+					ry+=littleNode->getRotate_array()[n_rotate]->getValue()[3];
+				if(littleNode->getRotate_array()[n_rotate]->getValue()[2]==1)
+					rz+=littleNode->getRotate_array()[n_rotate]->getValue()[3];
+			}
+
+			cout << "p3d_set_prim_pos " << littleNode->getID() << " " << x << " " << y << " " << z << " " << rx << " "<< ry << " " << rz << endl;
+			p3d_poly* poly= p3d_poly_get_poly_by_name ((char*)littleNode->getID());
+			p3d_set_prim_pos(poly, x, y, z, rx, ry, rz);
+	        p3d_scale_prim(poly, scale);
+
+		}
+	}
+
+	// Parcours récursif pour tous les sous-noeuds
+	for(size_t i=0; i<littleNode->getNode_array().getCount(); i++)
+	{
+		domNodeRef underLittleNode = littleNode->getNode_array()[i];
+		extractAndFillGeometryPositionColor(underLittleNode);
+	}
 }
 
-void p3d_collada_loader::extractVertices(const domVerticesRef vertices)
+void p3d_collada_loader::extractAndFillVertices(const domVerticesRef vertices)
 {
 	for (size_t l=0; l<vertices->getInput_array().getCount(); l++)
 	{
@@ -206,9 +261,9 @@ void p3d_collada_loader::extractVertices(const domVerticesRef vertices)
 	}
 }
 
-void p3d_collada_loader::extractPolylist(const domPolylistRef polylist)
+void p3d_collada_loader::extractAndFillPolylist(const domPolylistRef polylist)
 {
-	// On récupère l'offset des vertex
+	// Récupère l'offset des vertex
 	size_t vertexoffset = -1;
 	size_t polylist_stride = 0;
 	for (unsigned int w=0; w<polylist->getInput_array().getCount(); w++) {
@@ -225,13 +280,13 @@ void p3d_collada_loader::extractPolylist(const domPolylistRef polylist)
 	polylist_stride++;
 
 	int repere=0;
-	// On parcours tous les faces
+	// Parcourt toutes les faces
 	int * listeV = NULL;
 	for(size_t m=0; m<polylist->getVcount()->getValue().getCount(); m++)
 	{
 		cout << "p3d_add_desc_face ";
 
-		// On parcours tous les vertices des faces
+		// Parcourt toutes les vertices des faces
 		size_t num_verts = polylist->getVcount()->getValue()[m];
 
 		listeV = new int[num_verts];
@@ -250,9 +305,9 @@ void p3d_collada_loader::extractPolylist(const domPolylistRef polylist)
 	}
 }
 
-void p3d_collada_loader::extractTriangles(const domTrianglesRef triangles)
+void p3d_collada_loader::extractAndFillTriangles(const domTrianglesRef triangles)
 {
-	// On récupère l'offset des vertex
+	// Récupère l'offset des vertex
 	size_t vertexoffset = -1;
 	size_t triangles_stride = 0;
 	for (unsigned int w=0; w<triangles->getInput_array().getCount(); w++) {
@@ -269,13 +324,13 @@ void p3d_collada_loader::extractTriangles(const domTrianglesRef triangles)
 	triangles_stride++;
 
 	int repere=0;
-	// On parcours tous les faces
+	// Parcourt toutes les faces
 	int * listeV = NULL;
 	for(size_t m=0; m<triangles->getCount(); m++)
 	{
 		cout << "p3d_add_desc_face ";
 
-		// On parcours tous les vertices des faces
+		// Parcourt toutes les vertices des faces
 		size_t num_verts = 3; //comme c'est un triangle, il y a en a tj 3
 		listeV = new int[num_verts];
 		for(size_t n=0; n<num_verts; n++)
@@ -293,89 +348,29 @@ void p3d_collada_loader::extractTriangles(const domTrianglesRef triangles)
 	}
 }
 
-void p3d_collada_loader::extractGeometryMaterialPosition(const domNodeRef littleNode)
+void p3d_collada_loader::extractAndFillColor(const domMaterialRef pmat, const domNodeRef littleNode )
 {
-	// On récupère les instances géométriques
-	for(size_t k=0; k<littleNode->getInstance_geometry_array().getCount(); k++)
-	{
-		// On affiche des informations sur ce noeud
-		cout << "p3d_add_desc_poly " << littleNode->getID() << " P3D_GRAPHIC" << endl;
-		p3d_add_desc_poly((char *)littleNode->getID(),P3D_GRAPHIC);
-
-		domInstance_geometryRef instance_geo = littleNode->getInstance_geometry_array()[k];
-
-		// Grâce à l'URL, on va directement à la description de la géométrie dans la library_geometries
-		domGeometryRef geometry = daeSafeCast<domGeometry>(instance_geo->getUrl().getElement().cast());
-
-		//cout << geometry->getID() << endl;
-
-		if(geometry->getMesh())
-		{
-			// On récupère les vertices
-			domVerticesRef vertices = geometry->getMesh()->getVertices();
-			extractVertices(vertices);
-
-			// On récupère les polylist
-			for(size_t l=0; l<geometry->getMesh()->getPolylist_array().getCount(); l++)
-			{
-				domPolylistRef polylist = geometry->getMesh()->getPolylist_array()[l];
-				extractPolylist(polylist);
-			}
-			// On récupère les triangles
-			for(size_t l=0; l<geometry->getMesh()->getTriangles_array().getCount(); l++)
-			{
-				domTrianglesRef triangles = geometry->getMesh()->getTriangles_array()[l];
-				extractTriangles(triangles);
-			}
-			cout << "p3d_end_desc_poly" << endl;
-			p3d_end_desc_poly();
-
-			// TODO à revoir : pour l'instant c'est juste un test
-			// On ajoute le matérial de cette instance_geometry
-			if (!!instance_geo->getBind_material() && !!instance_geo->getBind_material()->getTechnique_common()) {
-				const domInstance_material_Array& matarray = instance_geo->getBind_material()->getTechnique_common()->getInstance_material_array();
-				for (size_t imat = 0; imat < matarray.getCount(); ++imat) {
-					domMaterialRef p_mat = daeSafeCast<domMaterial>(matarray[imat]->getTarget().getElement());
-					if (!!p_mat) {
-						fillGeometryColor(p_mat, littleNode);
-					}
-				}
-			}
-		}
-		// On ajoute la position du littleNode
-		if(littleNode)
-		{
-			float x=0, y=0, z=0;
-			float rx=0, ry=0, rz=0;
-			// translation
-			for(size_t n_translate=0; n_translate < littleNode->getTranslate_array().getCount(); n_translate++)
-			{
-				x+=littleNode->getTranslate_array()[n_translate]->getValue()[0];
-				y+=littleNode->getTranslate_array()[n_translate]->getValue()[1];
-				z+=littleNode->getTranslate_array()[n_translate]->getValue()[2];
-			}
-			//rotation
-			for(size_t n_rotate=0; n_rotate < littleNode->getRotate_array().getCount(); n_rotate++)
-			{
-				if(littleNode->getRotate_array()[n_rotate]->getValue()[0]==1)
-					rx+=littleNode->getRotate_array()[n_rotate]->getValue()[3];
-				if(littleNode->getRotate_array()[n_rotate]->getValue()[1]==1)
-					ry+=littleNode->getRotate_array()[n_rotate]->getValue()[3];
-				if(littleNode->getRotate_array()[n_rotate]->getValue()[2]==1)
-					rz+=littleNode->getRotate_array()[n_rotate]->getValue()[3];
-			}
-
-			cout << "p3d_set_prim_pos " << littleNode->getID() << " " << x << " " << y << " " << z << " " << rx << " "<< ry << " " << rz << endl;
-			p3d_poly* poly= p3d_poly_get_poly_by_name ((char*)littleNode->getID());
-			p3d_set_prim_pos(poly, x, y, z, rx, ry, rz);
-	        p3d_scale_prim(poly, scale);
-
-		}
-	}
-
-	for(size_t i=0; i<littleNode->getNode_array().getCount(); i++)
-	{
-		domNodeRef underLittleNode = littleNode->getNode_array()[i];
-		extractGeometryMaterialPosition(underLittleNode);
-	}
+    if( !!pmat && !!pmat->getInstance_effect() ) {
+        domEffectRef peffect = daeSafeCast<domEffect>(pmat->getInstance_effect()->getUrl().getElement().cast());
+        if( !!peffect ) {
+            domProfile_common::domTechnique::domPhongRef pphong = daeSafeCast<domProfile_common::domTechnique::domPhong>(peffect->getDescendant(daeElement::matchType(domProfile_common::domTechnique::domPhong::ID())));
+            if( !!pphong ) {
+                //if( !!pphong->getAmbient() && !!pphong->getAmbient()->getColor() ) {
+                  //  geom.ambientColor = getVector4(pphong->getAmbient()->getColor()->getValue());
+                //}
+                if( !!pphong->getDiffuse() && !!pphong->getDiffuse()->getColor() ) {
+                    domFx_color& diffuseColor = pphong->getDiffuse()->getColor()->getValue();
+                    // TODO : question
+                    // A quoi sert le dernier indice des couleurs ??
+                    cout << "p3d_set_prim_color " << littleNode->getID() << " Any " << diffuseColor.get(0) << " " << diffuseColor.get(1) << " " << diffuseColor.get(2) << endl;
+            		p3d_poly *poly;
+            		double color_vect[3];
+            		poly = p3d_poly_get_poly_by_name ((char *)littleNode->getID());
+            		for(int i=0; i<3; i++)
+            			color_vect[i]=diffuseColor.get(i);
+            		p3d_poly_set_color(poly, Any, color_vect);
+                }
+            }
+        }
+    }
 }
--- p3d/proto/p3d_collada_loader.h
+++ p3d/proto/p3d_collada_loader.h
@@ -16,31 +16,86 @@
  *
  */
 
-
 #include <dae.h>
 #include <dom/domCOLLADA.h>
 
+
+ /*! \class p3d_collada_loader
+   * \brief Classe représentant le chargeur d'un fichier Collada 1.5
+   *
+   *  La classe gère le chargement d'un fichier Collada 1.5 dans les structures p3d
+   */
 class p3d_collada_loader{
 
 public :
 
+    /*!
+     *  \brief Constructeur de la classe p3d_collada_loader
+     */
 	p3d_collada_loader();
-	~p3d_collada_loader();
 
+    /*!
+     *  \brief Destructeur de la classe p3d_collada_loader
+     */
+	virtual ~p3d_collada_loader();
+
+    /*!
+     *  \brief Charge un fichier Collada 1.5 dans les structures p3d
+     *
+     *  \param filename : le fichier Collada à charger
+     *  \return true si le fichier a correctement été parsé et chargé, false sinon
+     */
 	bool load(const char* filename);
 
 private :
 
-	DAE* m_collada;
-	domCOLLADA* m_root;
-	float scale;
+	DAE* m_collada; /*!< Accès vers la librairie ColladaDOM*/
+	domCOLLADA* m_root; /*!< Noeud root du fichier Collada*/
+	float scale; /*!< Echelle de la scène du fichier Collada*/
 
-	bool extract();
-	void fillGeometryColor(const domMaterialRef pmat, const domNodeRef littleNode );
-	void extractVertices(const domVerticesRef vertices);
-	void extractPolylist(const domPolylistRef polylist);
-	void extractTriangles(const domTrianglesRef triangles);
-	void extractGeometryMaterialPosition(const domNodeRef node);
+    /*!
+     *  \brief Extrait les informations utiles et remplit les structures p3d
+     *
+     *	Extrait les informations utiles d'un fichier Collada et remplit les structures p3d associées
+     *
+     *  \return true si le fichier a correctement été parsé, false sinon
+     */
+	bool extractAndFill();
+
+    /*!
+     *  \brief Extrait les informations utiles et remplit les structures p3d d'un sous-noeud
+     *
+     *	Extrait les informations utiles d'un sous-noeud d'une scène visuelle (visual_scene/node/node) d'un fichier Collada et remplit les structures p3d associées
+     */
+	void extractAndFillGeometryPositionColor(const domNodeRef node);
+
+    /*!
+     *  \brief Extrait les couleurs et remplit les structures p3d d'un sous-noeud
+     *
+     *	Extrait les couleurs d'un sous-noeud d'une scène visuelle (visual_scene/node/node) et remplit les structures p3d associées
+     */
+	void extractAndFillColor(const domMaterialRef pmat, const domNodeRef littleNode );
+
+	/*!
+     *  \brief Extrait les sommets et remplit les structures p3d d'un sous-noeud
+     *
+     *	Extrait les sommets d'un sous-noeud d'une scène visuelle (visual_scene/node/node) et remplit les structures p3d associées
+     */
+	void extractAndFillVertices(const domVerticesRef vertices);
+
+	/*!
+     *  \brief Extrait les faces (polynomes) et remplit les structures p3d d'un sous-noeud
+     *
+     *	Extrait les faces (polynomes) d'un sous-noeud d'une scène visuelle (visual_scene/node/node) et remplit les structures p3d associées
+     */
+	void extractAndFillPolylist(const domPolylistRef polylist);
+
+	/*!
+     *  \brief Extrait les faces (triangles) et remplit les structures p3d d'un sous-noeud
+     *
+     *	Extrait les faces (triangles) d'un sous-noeud d'une scène visuelle (visual_scene/node/node) et remplit les structures p3d associées
+     */
+	void extractAndFillTriangles(const domTrianglesRef triangles);
 
 };
 
